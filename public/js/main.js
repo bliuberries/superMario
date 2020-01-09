6f1637aa9cc9d7a2906fec7563c63fb660aa8e90
Brady <brady.y.liu@gmail.com>
2020-01-08 17:16:15 -0800

    Refactored code

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,28 @@ import { createBackgroundLayer } from './layers.js'
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(sprite, pos) {
+function createSpriteLayer(entity) {
   return function drawSpriteLayer(context) {
-    for(let i = 0; i < 20; ++i) {
-      sprite.draw('idle', context, pos.x + i * 16, pos.y);
-    }
+    entity.draw(context);
   };
+}
+
+class Vec2 {
+  constructor(x, y) {
+    this.set(x, y)
+  }
+
+  set(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Entity {
+  constructor() {
+    this.pos = new Vec2(0, 0);
+    this.vel = new Vec2(0, 0);
+  }
 }
 
 Promise.all([
@@ -19,26 +35,35 @@ Promise.all([
   loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-.then(([marioSprite, backGroundSprites, level]) => {
-  const comp = new Compositor();
+  .then(([marioSprite, backGroundSprites, level]) => {
+    const comp = new Compositor();
 
-  const backgroundLayer = createBackgroundLayer(level.backgrounds, backGroundSprites);
-  comp.layers.push(backgroundLayer);
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backGroundSprites);
+    comp.layers.push(backgroundLayer);
 
-  const pos = {
-    x: 64,
-    y: 64,
-  };
+    const gravity = 0.5;
 
-  const spriteLayer = createSpriteLayer(marioSprite, pos);
-  comp.layers.push(spriteLayer);
+    const mario = new Entity();
+    mario.pos.set(64, 180);
+    mario.vel.set(2, -10);
+    
+    mario.draw = function drawMario(context) {
+      marioSprite.draw('idle', context, this.pos.x, this.pos.y);
+    }
+    
+    mario.update = function updateMario() {
+      this.pos.x += this.vel.x;
+      this.pos.y += this.vel.y;
+    }
 
-  function update() {
-    comp.draw(context);
+    const spriteLayer = createSpriteLayer(mario);
+    comp.layers.push(spriteLayer);
 
-    pos.x += 2;
-    pos.y += 2;
-    requestAnimationFrame(update);
-  }
-  update();
-})
+    function update() {
+      comp.draw(context);
+      mario.update();
+      mario.vel.y += gravity;
+      requestAnimationFrame(update);
+    }
+    update();
+  })
