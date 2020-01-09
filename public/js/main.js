2fc3fb52262560fb1edb04523a06bac21c2936ad
Brady <brady.y.liu@gmail.com>
2020-01-08 17:31:36 -0800

    Cleaned up code

--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,24 @@
+import Compositor from './Compositor.js';
 import { loadLevel } from './loaders.js';
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
-import Compositor from './compositor.js'
-import { createBackgroundLayer } from './layers.js'
+import { createMario } from './entities.js'; 
+import { loadBackgroundSprites } from './sprites.js'; 
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(entity) {
-  return function drawSpriteLayer(context) {
-    entity.draw(context);
-  };
-}
-
-class Vec2 {
-  constructor(x, y) {
-    this.set(x, y)
-  }
-
-  set(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Entity {
-  constructor() {
-    this.pos = new Vec2(0, 0);
-    this.vel = new Vec2(0, 0);
-  }
-}
-
 Promise.all([
-  loadMarioSprite(),
+  createMario(),
   loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-  .then(([marioSprite, backGroundSprites, level]) => {
+  .then(([mario, backGroundSprites, level]) => {
     const comp = new Compositor();
 
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backGroundSprites);
-    comp.layers.push(backgroundLayer);
+    // comp.layers.push(backgroundLayer);
 
     const gravity = 0.5;
-
-    const mario = new Entity();
-    mario.pos.set(64, 180);
-    mario.vel.set(2, -10);
-    
-    mario.draw = function drawMario(context) {
-      marioSprite.draw('idle', context, this.pos.x, this.pos.y);
-    }
-    
-    mario.update = function updateMario() {
-      this.pos.x += this.vel.x;
-      this.pos.y += this.vel.y;
-    }
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
@@ -63,7 +27,9 @@ Promise.all([
       comp.draw(context);
       mario.update();
       mario.vel.y += gravity;
-      requestAnimationFrame(update);
+      // requestAnimationFrame(update);
+
+      setTimeout(update, 1000/60);
     }
     update();
   })
