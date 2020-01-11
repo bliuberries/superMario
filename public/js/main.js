94481da110e5269c8ef66b5627824d8dd505edb1
Brady <brady.y.liu@gmail.com>
2020-01-10 21:23:41 -0800

    Removed debugging tools

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 import Timer from './timer.js';
 import { loadLevel } from './loaders.js';
 import { createMario } from './entities.js';
-import { createCollisionLayer } from './layers.js';
 import { setupKeyboard } from './Input.js';
 
 
@@ -15,21 +14,11 @@ Promise.all([
   .then(([mario, level]) => {
     mario.pos.set(64, 64);
 
-    level.comp.layers.push(createCollisionLayer(level));
-
     level.entities.add(mario);
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
-    ['mousedown', 'mousemove'].forEach(eventName => {
-      canvas.addEventListener(eventName, event => {
-        if (event.buttons === 1) {
-          mario.vel.set(0, 0);
-          mario.pos.set(event.offsetX, event.offsetY);
-        }
-      })
-    })
     const timer = new Timer(1 / 60);
     timer.update = function (deltaTime) {
       level.update(deltaTime);
