cf9015b3e68b62465b3c0fa0ebd9e675e062d926
Brady <brady.y.liu@gmail.com>
2020-01-12 13:51:08 -0800

    Added horizontal movement to mario

--- public/js/entities.js
+++ public/js/entities.js
@@ -8,14 +8,14 @@ export function createMario() {
   return loadMarioSprite()
     .then(sprite => {
       const mario = new Entity();
-      mario.size.set(32, 32);
+      mario.size.set(14, 16);
       
       mario.addTrait(new Go());
       mario.addTrait(new Jump());
       // mario.addTrait(new Velocity());
 
       mario.draw = function drawMario(context) {
-        sprite.draw('idle', context, this.pos.x, this.pos.y);
+        sprite.draw('idle', context, 0, 0);
       }
 
       return mario;
