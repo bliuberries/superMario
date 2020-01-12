a6a0f9595d3378f84b86a72b1e6c1a62a307ddce
Brady <brady.y.liu@gmail.com>
2020-01-12 13:51:54 -0800

    Added spriteLayer

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,6 +1,6 @@
 export function createBackgroundLayer(level, sprites) {
   const buffer = document.createElement('canvas');
-  buffer.width = 256;
+  buffer.width = 2048;
   buffer.height = 240;
 
   const context = buffer.getContext('2d');
@@ -10,50 +10,64 @@ export function createBackgroundLayer(level, sprites) {
   })
 
   return function drawBackgroundLayer(context) {
-    context.drawImage(buffer, 0, 0)
+    context.drawImage(buffer, -camera.pos.x, -camera.pos.y);
   }
 }
 
-export function createSpriteLayer(entities) {
-  return function drawSpriteLayer(context) {
+export function createSpriteLayer(entities, width = 64, height = 64) {
+  const spriteBuffer = document.createElement('canvas');
+  spriteBuffer.width = width;
+  spriteBuffer.height = height;
+  const spriteBufferContext = spriteBuffer.getContext('2d');
+
+  return function drawSpriteLayer(context, camera) {
     entities.forEach(entity => {
-      entity.draw(context);
+      spriteBufferContext.clearRect(0, 0, width, height);
+
+      entity.draw(spriteBufferContext);
+
+      context.drawImage(
+        spriteBuffer,
+        entity.pos.x - camera.pos.x,
+        entity.pos.y - camera.pos.y
+      )
     })
   };
 }
 
-// export function createCollisionLayer(level) {
-//   const resolvedTiles = [];
+export function createCollisionLayer(level) {
+  const resolvedTiles = [];
 
-//   const tileResolver = level.tileCollider.tiles;
-//   const tileSize = tileResolver.tileSize;
+  const tileResolver = level.tileCollider.tiles;
+  const tileSize = tileResolver.tileSize;
 
-//   const getByIndexOriginal = tileResolver.getByIndex;
-//   tileResolver.getByIndex = function getByIndexFake(x, y) {
-//     resolvedTiles.push({ x, y });
-//     return getByIndexOriginal.call(tileResolver, x, y);
-//   }
+  const getByIndexOriginal = tileResolver.getByIndex;
+  tileResolver.getByIndex = function getByIndexFake(x, y) {
+    resolvedTiles.push({ x, y });
+    return getByIndexOriginal.call(tileResolver, x, y);
+  }
 
-//   return function drawCollision(context) {
-//     context.strokeStyle = 'blue';
-//     resolvedTiles.forEach(({ x, y }) => {
-//       context.beginPath();
-//       context.rect(
-//         x * tileSize,
-//         y * tileSize,
-//         tileSize, tileSize);
-//       context.stroke();
-//     });
+  return function drawCollision(context, camera) {
+    context.strokeStyle = 'blue';
+    resolvedTiles.forEach(({ x, y }) => {
+      context.beginPath();
+      context.rect(
+        x * tileSize - camera.pos.x,
+        y * tileSize - camera.pos.y,
+        tileSize, tileSize);
+      context.stroke();
+    });
 
-//     context.strokeStyle = 'red';
-//     level.entities.forEach(entity => {
-//       context.beginPath();
-//       context.rect(
-//         entity.pos.x, entity.pos.y,
-//         entity.size.x, entity.size.y);
-//       context.stroke();
-//     });
+    context.strokeStyle = 'red';
+    level.entities.forEach(entity => {
+      context.beginPath();
+      context.rect(
+        entity.pos.x - camera.pos.x,
+        entity.pos.y - camera.pos.y,
+        entity.size.x, entity.size.y);
+      context.stroke();
+    });
 
-//     resolvedTiles.length = 0;
-//   };
-// }
+    resolvedTiles.length = 0;
+  };
+}
