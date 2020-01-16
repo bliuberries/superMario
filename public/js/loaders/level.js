7c4db1f06ba861d7d190ce378f7fedce6d606435
Brady <brady.y.liu@gmail.com>
2020-01-16 12:05:54 -0800

    Refactored utilizing generator function/yields

--- public/js/loaders/level.js
+++ public/js/loaders/level.js
@@ -92,16 +92,12 @@ function expandRange(range) {
 
 function* expandRanges(ranges) {
   for (const range of ranges) {
-    for (const item of expandRange(range)) {
-      yield item;
-    }
+    yield* expandRange(range);
   }
 }
 
-function expandTiles(tiles, patterns) {
-  const expandedTiles = [];
-
-  function walkTiles(tiles, offsetX, offsetY) {
+function* expandTiles(tiles, patterns) {
+  function* walkTiles(tiles, offsetX, offsetY) {
     for (const tile of tiles) {
       for (const { x, y } of expandRanges(tile.ranges)) {
         const derivedX = x + offsetX;
@@ -109,19 +105,17 @@ function expandTiles(tiles, patterns) {
 
         if (tile.pattern) {
           const tiles = patterns[tile.pattern].tiles;
-          walkTiles(tiles, derivedX, derivedY);
+          yield* walkTiles(tiles, derivedX, derivedY);
         } else {
-          expandedTiles.push({
+          yield {
             tile,
             x: derivedX,
             y: derivedY
-          })
+          };
         }
       }
     }
   }
 
-  walkTiles(tiles, 0, 0);
-
-  return expandedTiles;
+  yield* walkTiles(tiles, 0, 0);
 }
