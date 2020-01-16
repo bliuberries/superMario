9bc32f97639a0c64bcc55854fe543035121d44e6
Brady <brady.y.liu@gmail.com>
2020-01-16 15:11:24 -0800

    Added additional goomba behavior

--- public/js/entities/Goomba.js
+++ public/js/entities/Goomba.js
@@ -1,4 +1,5 @@
-import Entity from '../entity.js';
+import Entity, { Sides, Trait } from '../entity.js';
+import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js';
 import { loadSpriteSheet } from '../loaders.js';
 
@@ -7,12 +8,29 @@ export function loadGoomba() {
   .then(createGoombaFactory);
 }
 
-function createGoombaFactory(sprite) {
+class Behavior extends Trait {
+  constructor() {
+    super('behavior');
+  }
 
+  collides(us, them) {
+    if(them.stomper) {
+      us.killable.kill();
+      us.PendulumWalk.speed = 0;
+    }
+  }
+}
+
+function createGoombaFactory(sprite) {
   const walkAnim = sprite.animations.get('walk');
 
+  function routeAnim(goomba) {
+    if(goomba.killable.dead) return 'flat';
+    return walkAnim(goomba.lifetime);
+  }
+
   function drawGoomba(context) {
-    sprite.draw(walkAnim(this.lifetime), context, 0, 0);
+    sprite.draw(routeAnim(this), context, 0, 0);
   }
 
   return function createGoomba() {
@@ -20,6 +38,8 @@ function createGoombaFactory(sprite) {
     goomba.size.set(16, 16);
 
     goomba.addTrait(new PendulumWalk());
+    goomba.addTrait(new Behavior);
+    goomba.addTrait(new Killable());
 
     goomba.draw = drawGoomba;
 
