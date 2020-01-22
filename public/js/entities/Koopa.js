95dbe13e6c1dd95ae308325181101696b9976a7f
Brady <brady.y.liu@gmail.com>
2020-01-21 19:48:18 -0800

    Added wake animation, shell flying across map animation, refactored code

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,15 +1,16 @@
-import Entity, { Sides, Trait} from '../entity.js';
+import Entity, { Sides, Trait } from '../entity.js';
 import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js'
 import { loadSpriteSheet } from '../loaders.js';
 
 export function loadKoopa() {
   return loadSpriteSheet('koopa')
-  .then(createKoopaFactory);
+    .then(createKoopaFactory);
 }
 
 const STATE_WALKING = Symbol('walking');
 const STATE_HIDING = Symbol('hiding');
+const STATE_PANIC = Symbol('panic');
 
 class Behavior extends Trait {
   constructor() {
@@ -17,46 +18,77 @@ class Behavior extends Trait {
     this.state = STATE_WALKING;
     this.hideTime = 0;
     this.hideDuration = 5;
+
+    this.walkSpeed = null;
+    this.panicSpeed = 300;
   }
 
   collides(us, them) {
-    if(us.killable.dead) return;
+    if (us.killable.dead) return;
 
-    if(them.stomper) {
-      if(them.vel.y > us.vel.y) {
+    if (them.stomper) {
+      if (them.vel.y > us.vel.y) {
         this.handleStomp(us, them);
       } else {
-        them.killable.kill();
+        this.handleNudge(us, them);
       }
     }
   }
 
+  handleNudge(us, them) {
+    if (this.state === STATE_WALKING) {
+      them.killable.kill();
+    } else if (this.state === STATE_HIDING) {
+      this.panic(us, them);
+    } else if (this.state === STATE_PANIC) {
+      const travelDir = Math.sign(us.vel.x);
+      const impactDir = Math.sign(us.pos.x - them.pos.x);
+      if(travelDir !== 0 && travelDir !== impactDir) {
+        them.killable.kill();
+      }
+    }
+
+  }
+
   handleStomp(us, them) {
-    if(this.state === STATE_WALKING) {
+    if (this.state === STATE_WALKING) {
       this.hide(us);
     } else if (this.state === STATE_HIDING) {
       us.killable.kill();
       us.vel.set(100, -200);
       us.canCollide = false;
+    } else if (this.state === STATE_PANIC) {
+      this.hide(us);
+
     }
   }
 
   hide(us) {
     us.vel.x = 0;
-    us.PendulumWalk.enabled = false;
+    us.pendulumWalk.enabled = false;
+    if(this.walkSpeed === null) {
+      this.walkSpeed = us.pendulumWalk.walkSpeed;
+    }
     this.hideTime = 0;
     this.state = STATE_HIDING;
   }
 
   unhide(us) {
-    us.PendulumWalk.enabled = true;
+    us.pendulumWalk.enabled = true;
+    us.pendulumWalk.walkSpeed = this.walkSpeed;
     this.state = STATE_WALKING;
   }
 
+  panic(us, them) {
+    us.pendulumWalk.enabled = true;
+    us.pendulumWalk.speed = this.panicSpeed * Math.sign(them.vel.x);
+    this.state = STATE_PANIC;
+  }
+
   update(us, deltaTime) {
-    if(this.state === STATE_HIDING) {
+    if (this.state === STATE_HIDING) {
       this.hideTime += deltaTime;
-      if(this.hideTime > this.hideDuration) {
+      if (this.hideTime > this.hideDuration) {
         this.unhide(us)
       }
     }
@@ -65,9 +97,20 @@ class Behavior extends Trait {
 
 function createKoopaFactory(sprite) {
   const walkAnim = sprite.animations.get('walk');
+  const wakeAnim = sprite.animations.get('wake');
 
   function routeAnim(koopa) {
-    if(koopa.behavior.state === STATE_HIDING) return 'hiding';
+    if (koopa.behavior.state === STATE_HIDING) {
+      if(koopa.behavior.hideTime > 3) {
+        return wakeAnim(koopa.behavior.hideTime)
+      }
+      return 'hiding';
+    }
+
+    if(koopa.behavior.state === STATE_PANIC) {
+      return 'hiding';
+    }
+
     return walkAnim(koopa.lifetime);
   }
 
