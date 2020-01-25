082650271793da96776f6390c06e8a791e011961
Brady <brady.y.liu@gmail.com>
2020-01-24 16:19:01 -0800

    Formating for webpack config

--- public/js/anim.js
+++
@@ -1,7 +0,0 @@
-export function createAnim(frames, frameLen) {
-  return function resolveFrame(distance) {
-    const frameIndex = distance / frameLen % frames.length | 0;
-    const frameName = frames[frameIndex];
-    return frameName
-  }
-};
